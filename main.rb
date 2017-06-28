5f8135c03c43d36c6e351c6fe3f11677074c2542
Hugo Morales <hmoralesg@icloud.com>
2017-06-28 13:43:04 -0400

    Finalizado menu de opciones, falta hacer los metodos que hacen el trabajo

--- main.rb
+++ main.rb
@@ -1,7 +1,13 @@
+# Programa de calculo de notas en base a un archivo csv para Desafio Latam
+# 2017 - Hugo Morales
+
+require 'io/console'
+
 def main(file)
   menu(file)
 end
 
+# Menus
 def menu(file)
   puts dialogo_bienvenida
   opcion = 0
@@ -9,7 +15,6 @@ def menu(file)
     opcion = menu_principal
     motor_seleccion(opcion, file)
   end
-  puts dialogo_salida
 end
 
 def menu_principal
@@ -17,19 +22,24 @@ def menu_principal
   while opcion < 1 || opcion > 4
     puts dialogo_menu
     opcion = gets.chomp.to_i
+    clear
   end
   opcion
 end
+# Fin seccion menus
 
+# Inicio seccion motor de opciones
 def motor_seleccion(opcion, file)
   case opcion
   when 1 then puts "Opcion 1 seleccionada #{file}"
   when 2 then puts "Opcion 2 seleccionada #{file}"
   when 3 then puts "Opcion 3 seleccionada #{file}"
-  when 4 then puts 'Opcion 4 seleccionada'
+  when 4 then dialogo_salida
   else puts 'Opcion seleccionada no valida'
   end
 end
+
+# Fin seccion motor de opciones
 
 # Seccion dialogos - Inicio
 def dialogo_bienvenida
@@ -39,13 +49,29 @@ def dialogo_bienvenida
 end
 
 def dialogo_menu
-  # TO-DO hacer el dialogo del menu
+  'Ingrese una opcion:
+1) Generar archivo con promedios de cada alumno.
+2) Mostrar total de inasistencias.
+3) Mostrar alumnos aprobados.
+4) Salir de este programa.'
 end
 
 def dialogo_salida
-  # TO-DO Crear dialogo salida
+  clear
+  puts 'Gracias por utilizar este programa'
+  presione_para_continuar
 end
 
 # Seccion dialogos - Fin
+
+# Metodos auxiliares
+def presione_para_continuar
+  puts '[Presione cualquier tecla para continuar]'
+  STDIN.getch
+end
+
+def clear
+  system 'clear'
+end
 
 main('notas.csv')
