ae29b66b02e3c063099cf75eca1ee3667d87dbfc
Hugo Morales <hmoralesg@icloud.com>
2017-06-29 09:52:01 -0400

    Funcionando la funcion de calculo de promedios :)

--- main.rb
+++ main.rb
@@ -9,6 +9,7 @@ end
 
 # Menus
 def menu(file)
+  clear
   puts dialogo_bienvenida
   opcion = 0
   while opcion != 4
@@ -31,7 +32,7 @@ end
 # Inicio seccion motor de opciones
 def motor_seleccion(opcion, file)
   case opcion
-  when 1 then puts "Opcion 1 seleccionada #{file}"
+  when 1 then puts opcion_uno(file)
   when 2 then puts "Opcion 2 seleccionada #{file}"
   when 3 then puts "Opcion 3 seleccionada #{file}"
   when 4 then dialogo_salida
@@ -39,7 +40,49 @@ def motor_seleccion(opcion, file)
   end
 end
 
+def opcion_uno(file)
+  data_array = procesar_data(file)
+  data_ordenada = hash_data(data_array)
+  promedios = calcular_promedios(data_ordenada)
+  promedios.each_pair { |key, value| puts "#{key}: #{value}" }
+end
+
 # Fin seccion motor de opciones
+
+# Inicio seccion motor de calculo para opciones
+
+def procesar_data(file)
+  # Se le entrega un archivo y devuelve un array con sus datos separados.
+  file = File.open(file, 'r')
+  data = []
+  raw_data = file.readlines
+  raw_data.each { |x| data.push(x.split(', ').map(&:chomp)) }
+  data
+end
+
+def hash_data(data_array)
+  # Se le entrega un array y devuelve un Hash con los nombres y las notas.
+  data = {}
+  largo = data_array.length + 1
+  data_array.each { |x| data[x[0].to_sym] = x[1..largo + 1].map(&:to_i) }
+  data
+end
+
+def calcular_promedios(data)
+  # Se le entrega un hash con nombre y notas y devuelve un hash con promedios
+  # por nombre.
+  promedios = {}
+  data.each_pair { |key, value| promedios[key] = obtener_promedio(value) }
+  promedios
+end
+
+def obtener_promedio(notas)
+  # Se le entrega un array con notas y entrega el promedio.
+  suma_notas = notas.inject(0) { |sum, x| sum + x }
+  suma_notas / notas.length.to_f
+end
+
+# Fin Seccion motor de calculo
 
 # Seccion dialogos - Inicio
 def dialogo_bienvenida
@@ -60,6 +103,7 @@ def dialogo_salida
   clear
   puts 'Gracias por utilizar este programa'
   presione_para_continuar
+  clear
 end
 
 # Seccion dialogos - Fin
