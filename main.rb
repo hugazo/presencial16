f59a52b2e3f02377bf477c4532a00aff013fb9b8
Hugo Morales <hmoralesg@icloud.com>
2017-06-29 20:32:51 -0400

    Programa finalizado. Opcion 1 OK Opcion 2 OK Opcion 3 OK --- No se encuentran errores funcionales ni warnings de rubocop.

--- main.rb
+++ main.rb
@@ -13,16 +13,20 @@ def menu(file)
   puts dialogo_bienvenida
   opcion = 0
   while opcion != 4
-    opcion = menu_principal
+    opcion = menu_seleccion_principal
     motor_seleccion(opcion, file)
   end
 end
 
-def menu_principal
+def menu_seleccion_principal
+  # Muestra en pantalla las opciones de menu habilitadas y devuelve la
+  # opcion seleccionada como entero para luego ser ejecutada
   opcion = 0
   while opcion < 1 || opcion > 4
     puts dialogo_menu
-    opcion = gets.chomp.to_i
+    opcion = gets.chomp
+    opcion_no_valida(opcion) unless opcion.to_i >= 1 && opcion.to_i <= 4
+    opcion = opcion.to_i
     clear
   end
   opcion
@@ -31,19 +35,20 @@ end
 
 # Inicio seccion motor de opciones
 def motor_seleccion(opcion, file)
+  # Ejecuta la opcion seleccionada llamando a los metodos necesarios.
   case opcion
   when 1 then opcion_uno(file)
-  when 2 then puts "Opcion 2 seleccionada #{file}"
-  when 3 then puts "Opcion 3 seleccionada #{file}"
+  when 2 then opcion_dos(file)
+  when 3 then opcion_tres(file)
   when 4 then dialogo_salida
-  else puts 'Opcion seleccionada no valida'
   end
 end
 
 def opcion_uno(file)
+  # Ejecuta la opcion uno, crear un informe de promedio de notas, muestra los
+  # resultados en pantalla y consulta si debe guardar los datos mostrados.
   clear
-  data_array = procesar_data(file)
-  data_ordenada = hash_data(data_array)
+  data_ordenada = crear_array_notas(file)
   promedios = calcular_promedios(data_ordenada)
   imprimir_resultados(promedios)
   guardar_resultados(promedios)
@@ -51,11 +56,36 @@ def opcion_uno(file)
 end
 
 def opcion_dos(file)
+  # Ejecuta la opcion dos, crear un informe de inasistencias, muestra los
+  # resultados en pantalla y consulta si se debe guardar los datos mostrados.
+  clear
+  data_ordenada = crear_array_notas(file)
+  inasistencias = contar_inasistencias(data_ordenada)
+  imprimir_resultados(inasistencias)
+  guardar_resultados(inasistencias)
+  finalizar_ejecucion_opcion
+end
 
+def opcion_tres(file)
+  # Ejecuta la opcion tres, solicita una nota de aprobacion y luego muestra
+  # los alumnos que cumplen con esa nota y consulta si debe guardar.
+  clear
+  data_ordenada = crear_array_notas(file)
+  promedios = calcular_promedios(data_ordenada)
+  aprobados = obtener_aprobados(promedios)
+  imprimir_resultados(aprobados)
+  guardar_resultados(aprobados)
+  finalizar_ejecucion_opcion
 end
 # Fin seccion motor de opciones
 
 # Inicio seccion motor de calculo para opciones
+
+def crear_array_notas(file)
+  # Metodo que toma un archivo, luego llama a el metodo que genera el array y
+  # despues ese array lo procesa como Hash estandar de notas.
+  hash_data(procesar_data(file))
+end
 
 def procesar_data(file)
   # Se le entrega un archivo y devuelve un array con sus datos separados.
@@ -82,6 +112,54 @@ def calcular_promedios(data)
   promedios
 end
 
+def contar_inasistencias(data)
+  # Cuenta las inasistencias por alumno y luego llama al metodo que Cuenta
+  # el total de las inasistencias.
+  inasistencias = {}
+  data.each_pair { |key, value| inasistencias[key] = value.count(0) }
+  inasistencias[:Total] = total_inasistencias(inasistencias)
+  inasistencias
+end
+
+def obtener_aprobados(data)
+  # Llama a metodo auxiliar para obtener la nota de aprobacion, luego evalua
+  # quienes cumplen con dicha nota y llena el hash con los aprobados
+  nota_aprobacion = obtener_nota_aprobacion
+  aprobados = {}
+  data.each_pair { |key, val| aprobados[key] = val if val >= nota_aprobacion }
+  aprobados
+end
+
+def obtener_nota_aprobacion
+  # Muestra en pantalla la seleccion de la nota de aprobacion, evaluando
+  # correcto ingreso y luego devolviendo ese valor como decimal.
+  nota = 0
+  while nota < 1 || nota > 10
+    puts 'Ingrese nota de aprobación (5 por defecto)'
+    nota = gets.chomp
+    nota = nota.tr(',', '.')
+    nota = '5' if nota == ''
+    opcion_no_valida(nota) unless nota.to_f >= 1 && nota.to_f <= 10
+    nota = nota.to_f
+  end
+  nota
+end
+
+def opcion_no_valida(opcion)
+  # Metodo a ejecutar en caso de ingresar una opcion no valida. Muestra en
+  # pantalla un mensaje de error y la opcion ingresada.
+  clear
+  puts "Opción ingresada #{opcion} no es válida, intente nuevamente"
+  finalizar_ejecucion_opcion
+end
+
+def total_inasistencias(inasistencias)
+  # Cuenta el total de las inasistencias de un hash entregado.
+  total = 0
+  inasistencias.each_value { |value| total += value }
+  total
+end
+
 def obtener_promedio(notas)
   # Se le entrega un array con notas y entrega el promedio.
   suma_notas = notas.inject(0) { |sum, x| sum + x }
@@ -89,25 +167,33 @@ def obtener_promedio(notas)
 end
 
 def imprimir_resultados(hash)
+  # Crea una impresion para pantalla de un hash entregado
   hash.each_pair { |key, value| puts "#{key}: #{value}" }
 end
 
 def guardar_resultados(data)
+  # Se le pasa un hash y comienza una subrutina para guardar los resultados
+  # incluidos en ese hash
   opcion = nil
   while opcion != 's' && opcion != 'n'
     puts 'Desea guardar el resultado? (s/n)'
     opcion = gets.downcase.chomp
+    opcion_no_valida(opcion) if opcion != 'n' && opcion != 's'
   end
   ejecutar_opcion_guardado(opcion, data)
 end
 
 def ejecutar_opcion_guardado(opcion, data)
+  # Se llama desde guardar_resultados para poder procesar la opcion seleccionada
   dialogo_guardado_archivo(data) if opcion == 's'
-  puts 'No se guardará archivo' if opcion == 'n'
-  puts 'Opcion no valida' if opcion != 'n' && opcion != 's'
+  return unless opcion == 'n'
+  clear
+  puts 'No se guardará archivo'
 end
 
 def dialogo_guardado_archivo(hash)
+  # Se le pasa data en hash y guarda esa data en un archivo con extension csv
+  # luego comprueba si ese archivo fue correctamente guardado.
   puts "Ingrese el nombre de archivo\nSe utilizará la extensión .csv"
   filename = gets.chomp
   filename += '.csv'
@@ -118,6 +204,7 @@ def dialogo_guardado_archivo(hash)
 end
 
 def resultado_guardado(filename)
+  # Determina si un archivo se encuentra creado e imprime en pantalla su ruta.
   if File.exist?(filename)
     file = File.expand_path(filename)
     puts "Archivo #{file} guardado correctamente"
@@ -130,12 +217,14 @@ end
 
 # Seccion dialogos - Inicio
 def dialogo_bienvenida
+  # Retorna string de bienvenida al programa.
   '*******************************
 ** Programa de Gestion Notas **
 *******************************'
 end
 
 def dialogo_menu
+  # Retorna string de menu principal.
   'Ingrese una opcion:
 1) Generar archivo con promedios de cada alumno.
 2) Mostrar total de inasistencias.
@@ -144,6 +233,7 @@ def dialogo_menu
 end
 
 def dialogo_salida
+  # Muestra mensaje de despedida
   clear
   puts 'Gracias por utilizar este programa'
   presione_para_continuar
@@ -154,15 +244,20 @@ end
 
 # Metodos auxiliares
 def presione_para_continuar
+  # Pausa la ejecucion, mostrando mensaje de presionar tecla y al presionar una,
+  #  se continua la ejecucion del programa
   puts '[Presione cualquier tecla para continuar]'
   STDIN.getch
 end
 
 def clear
+  # Limpia la pantalla, metodo no funciona en Windows
   system 'clear'
 end
 
 def finalizar_ejecucion_opcion
+  # Muestra mensaje de presionar para continuar y limpia pantalla una vez
+  # presionada una tecla.
   presione_para_continuar
   clear
 end
